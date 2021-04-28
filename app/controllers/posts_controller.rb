5f281c480c4db21f2fa3203854c94373aa4a1701
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-28 13:28:52 +0900

    [Add] 投稿機能

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: '投稿しました'
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body, :video)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  resources :posts, only: %i(new create)
 end
